3bb5dde27b4c279ea771f221d3b3efa06f1e6a5f
suprvn <alexsupran98@gmail.com>
2021-10-27 07:44:57 +0300

    test5 upd

--- src/test/java/GetUsersTest.java
+++ src/test/java/GetUsersTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class GetUsersTest {
     private String url = DataManager.getTestData("Global.base_url") + DataManager.EndPoints.USERS.get();
-    Map<String, Object> user_5 = new HashMap<>();
+    Map<String, Object> user = new HashMap<>();
     private final String user_data = "src/test/test_data/User5.json";
     @Test
     public void getUsers() {
@@ -18,13 +18,13 @@ public class GetUsersTest {
         Assert.assertEquals(ApiUtils.getStatusCode(), 200, "Unexpected status code\n");
         Assert.assertEquals(ApiUtils.getContentType(), DataManager.EndPoints.TYPE_JSON.get(), "Unexpected content type\n");
 
-        ApiUtils.getWithParams(url, user_5);
+        ApiUtils.getWithParams(url, user);
         JSONArray response_arr = JsonUtils.getJsonArray(ApiUtils.getResponseBody());
         Assert.assertEquals(response_arr.size(), 1, "Wrong returned array's size\n"); // the only element should be found
         Assert.assertEquals(response_arr.get(0), JsonUtils.convertToJson(DataManager.read(user_data)), "Unexpected response body\n");
     }
     @BeforeTest
     private void fillId() {
-        user_5.put("id", "5");
+        user.put("id", DataManager.getTestData("GetUsersTest.user_id"));
     }
 }
